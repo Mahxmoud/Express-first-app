443a112d35100e8164bb77ebd77f8742404c08de
mahxmoud <mahmoud.jomaa@yahoo.com>
2021-01-09 18:22:11 +0200

    added app.js

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('home page')
+    res.sendFile(__dirname + '/views/index.html')
+})
+app.get('/services', (req, res) => {
+    res.sendFile(__dirname + '/views/services.html')
+})
+app.get('/contact', (req, res) => {
+    res.sendFile(__dirname + '/views/contact.html')
 })
 
 app.listen(3000)
